3a6d381de40719f4c576d51f33c01953bb915139
a-ridley <abixcastro@gmail.com>
2020-10-07 15:12:27 -0700

    fix permission error

--- chaos-experiments/experiment3.sh
+++ chaos-experiments/experiment3.sh
@@ -7,4 +7,4 @@ CIRCLE_USERNAME=$3
 
 
 
-curl -i -X POST 'https://api.gremlin.com/v1/attacks/new?teamId=a03d53c4-a331-5cc9-a94b-1815aba199f5' -H 'Content-Type: application/json;charset=utf-8' -H 'Authorization: Bearer Yy0zODZkNjNhMS1kMGI1LTU2ODYtYTdkZi1hMjA2OTRiYTBlNmI6Y29tbXVuaXR5K2NpcmNsZUBncmVtbGluLmNvbToxYzA5MWZhYS04NTdlLTQzMzEtODkxZi1hYTg1N2UzMzMxOGQ=' -d '{"target":{"type":"Random","containers":{"multiSelectLabels":{"app":["7777777-cicd-chaos-app"]}},"percent":100},"command":{"type":"","commandType":"","args":["-d","1","-r"]}}'
+curl -i -X POST "https://api.gremlin.com/v1/attacks/new?teamId=${GREMLIN_TEAM_ID}" -H 'Content-Type: application/json;charset=utf-8' -H "Authorization: Key ${GREMLIN_API_KEY}" -d '{"target":{"type":"Random","containers":{"multiSelectLabels":{"io.kubernetes.container.name":["'"$CIRCLE_USERNAME-cicd-chaos-app"'"]}},"percent":100},"command":{"type":"cpu","commandType":"CPU","args":["-l","120","-p","60","-a"]}}'
