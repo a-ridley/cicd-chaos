d8d3a99e9e37a8a317eef5e0a8fb5a04e05d9230
a-ridley <abixcastro@gmail.com>
2020-10-07 15:35:36 -0700

    place curl command in experiment 2 file

--- chaos-experiments/experiment2.sh
+++ chaos-experiments/experiment2.sh
@@ -1,5 +1,5 @@
 ########################################
-# Experiment #2 - Testing non-critical application adservice
+# Experiment #2 - Latency
 #
 
 GREMLIN_TEAM_ID=$1
@@ -7,4 +7,4 @@ GREMLIN_API_KEY=$2
 CIRCLE_USERNAME=$3
 
 
-curl -i -X POST "https://api.gremlin.com/v1/attacks/new?teamId=${GREMLIN_TEAM_ID}" -H 'Content-Type: application/json;charset=utf-8' -H "Authorization: Key ${GREMLIN_API_KEY}" -d '{"target":{"type":"Random","containers":{"multiSelectLabels":{"io.kubernetes.container.name":["'"$CIRCLE_USERNAME-cicd-chaos-app"'"]}},"percent":100},"command":{"type":"cpu","commandType":"CPU","args":["-l","120","-p","60","-a"]}}'
+curl -i -X POST "https://api.gremlin.com/v1/attacks/new?teamId=${GREMLIN_TEAM_ID}" -H 'Content-Type: application/json;charset=utf-8' -H "Authorization: Key ${GREMLIN_API_KEY}" -d '{"target":{"type":"Random","containers":{"multiSelectLabels":{"app":["["'"$CIRCLE_USERNAME-cicd-chaos-app"'"]}}"]}},"percent":100},"command":{"type":"latency","commandType":"Latency","args":["-l","300","-m","500","-h","^api.gremlin.com","-p","^53"],"providers":[]}}'
